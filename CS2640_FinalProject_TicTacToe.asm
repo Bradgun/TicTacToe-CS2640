f034a9dba734091af89cf22f796fcb9f1270b5fb
Elsa Zheng <zhengelsa150@gmail.com>
2024-11-23 22:06:11 -0800

    Set up
    The good practices stuff (header), .data, .text, main:

---
+++ CS2640_FinalProject_TicTacToe.asm
@@ -0,0 +1,12 @@
+#Bradley Gunawan, Elsa Zheng, Joe Jhenson Ramos, Kenneth Wang, Manuel Abanto
+#Group 4
+#CS 2640.02 with Professor Cariaga
+#Final Project: Tic Tac Toe
+#Goal/Objective: Create a functional and good looking Tic Tac Toe game
+#Requirements: Good UI, update board with each move, winning/ending condition (3 in a row or draw),
+#Alternating loop for player turns, check for correct input
+
+.data
+
+.text
+main:

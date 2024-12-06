10b91dc819a1c676cbc7a1b9bb3d6c3ab1ac6d89
Elsa Zheng <zhengelsa150@gmail.com>
2024-12-06 13:20:38 -0800

    Input Validation
    Forces player to choose a different spot if spot is already taken

--- CS2640_FinalProject_TicTacToe.asm
+++ CS2640_FinalProject_TicTacToe.asm
@@ -1,126 +1,201 @@
-# Bradley Gunawan, Elsa Zheng, Joe Jhenson Ramos, Kenneth Wang, Manuel Abanto
-# Group 4
-# CS 2640.02 with Professor Cariaga
-# Final Project: Tic Tac Toe
-# Goal/Objective: Create a functional and good looking Tic Tac Toe game
-# Requirements: Good UI, update board with each move, winning/ending condition (3 in a row or draw),
-# For board, label each tile with numbers 1-9, and replace with X/O depending on user input
-# Alternating loop for player turns, check for correct input
+#Bradley Gunawan, Elsa Zheng, Joe Jhenson Ramos, Kenneth Wang, Manuel Abanto
+#Group 4
+#CS 2640.02 with Professor Cariaga
+#Final Project: Tic Tac Toe
+#Goal/Objective: Create a functional and good looking Tic Tac Toe game
+#Requirements: Good UI, update board with each move, winning/ending condition (3 in a row or draw),
+#For board, label each tile with numbers 1-9, and replace with X/O depending on user input
+#Alternating loop for player turns, check for correct input
+#For board, implement with a 1d array with 9 * 4 byte representing a 3x3 board
 
-# For board, implement with a 1d array with 9 * 4 byte representing a 3x3 board
+.macro printStr %str
+        li $v0, 4
+       	la $a0, %str
+       	syscall
+.end_macro
+    
+.macro printInt %int
+        li $v0, 1
+       	move $a0, %int
+       	syscall
+.end_macro
+
+.macro printBoard
+    	la $t0, board
+    	li $t1, 0
+   	loop:
+    		lw $t2, 0($t0)
+   		beq $t2, 0, printEmpty
+    		beq $t2, 1, printX
+    		beq $t2, 2, printO
+    		cont:
+    			addi $t0, $t0, 4
+    			addi $t1, $t1, 1
+    			beq $t1, 3, printNewline
+    			beq $t1, 6, printNewline
+    			blt $t1, 9, loop
+    		b end
+    			
+    	printEmpty:
+    		printStr empty
+    		b cont
+    	printX:
+    		printStr Xmark
+    		b cont
+    	printO:
+    		printStr Omark
+    		b cont
+    	printNewline:
+    		printStr newline
+    		b loop
+    	end:
+    	printStr newline
+.end_macro
+
 
 .data
-	    board: .word 0, 0, 1, 0, 1, 0, 1, 0, 0 	# Reserve 9 * 4 bytes (1D array for 3x3 board) 0: Empty, 1: X, 2: O
-    	# board: .space 36           # Too messy, need init at beginning for main
+	board: .word 0, 0, 0, 0, 0, 0, 0, 0, 0 	# Reserve 9 * 4 bytes (1D array for 3x3 board) 0: Empty, 1: X, 2: O
     	Xmark: .asciiz " X "
     	Omark: .asciiz " O "
     	empty: .asciiz "   "
 
-    	player1Win: .asciiz "Player 1 Wins!"
-    	player2Win: .asciiz "Player 2 Wins!"
-    	instructions: .asciiz "Player 1 is X and Player 2 is O. X (Player 1) goes first. Players take turn placing X's and O's until they reach three in a row (horizontally, vertically, or diagonally) to win or fill all the spaced to draw."
-    	prompt1: .asciiz "Player 1's move" #tell player 1 it's their turn
-    	promtp2: .asciiz "Player 2's move" #tell player 2 it's their turn
-    	playAgain: .asciiz "Do you wish to play again?"
+    	player1Win: .asciiz "\nPlayer 1 Wins!"
+    	player2Win: .asciiz "\nPlayer 2 Wins!"
+    	instructions: .asciiz "Player 1 is X and Player 2 is O. X (Player 1) goes first. \nPlayers take turn placing X's and O's until they reach three in a row (horizontally, vertically, or diagonally) to win or fill all the spaced to draw. \nEnter a number 1-9 that corresponds to the position you wish to put your piece in."
+    	prompt1: .asciiz "\nPlayer 1's move: " #tell player 1 it's their turn
+    	prompt2: .asciiz "\nPlayer 2's move: " #tell player 2 it's their turn
+    	playAgain: .asciiz "\nDo you wish to play again?: "
     	newline: .asciiz "\n"
-
-    	.macro printStr %str
-        	li $v0, 4
-        	la $a0, %str
-        	syscall
-    	.end_macro
-    
-    	.macro printInt %int
-        	li $v0, 1
-        	move $a0, %int
-        	syscall
-    	.end_macro
-
-    	.macro printBoard
-    		la $t0, board
-    		li $t1, 0
-    		loop:
-    			lw $t2, 0($t0)
-    			beq $t2, 0, printEmpty
-    			beq $t2, 1, printX
-    			beq $t2, 2, printO
-    			cont:
-    				addi $t0, $t0, 4
-    				addi $t1, $t1, 1
-    				beq $t1, 3, printNewline
-    				beq $t1, 6, printNewline
-    				blt $t1, 9, loop
-    			b end
-    			
-    		printEmpty:
-    			printStr empty
-    			b cont
-    		printX:
-    			printStr Xmark
-    			b cont
-    		printO:
-    			printStr Omark
-    			b cont
-    		printNewline:
-    			printStr newline
-    			b loop
-    		end:
-    		printStr newline
-	.end_macro
-
-
-
+    	wrongPosition: .asciiz "\nSpot already taken. Please choose a different spot"
 
 .text
 main:
-    # print instructions to users
-    # li $v0, 4
-    # la $a0, instructions
-    # syscall
+	#print instructions to users
+	li $v0, 4
+	la $a0, instructions
+	syscall
 
-    # Print Skeleton Sample Board
-    printBoard
+	#Print Skeleton Sample Board
+	printBoard
 
-    # Loop counter
-    # li $s0, 0
+	#Loop counter	
+	li $s0, 0
+	
+	#To mark the spot for player 1
+	li $s1, 1
+	 
+	#To mark the spot for player 1
+	li $s2, 2
 
-    # Jump to label player1 to begin game
-    # j player1
-    
-    exit:
-    # exit program
-    li $v0, 10
-    syscall
+        #Jump to label player1 to begin game
+        j player1
     
 player1:
-    # Player 1 input (integer)
-    li $v0, 5
-    syscall
+	#prompt player 1
+	li $v0, 4
+	la $a0, prompt1
+	syscall
+	
+	#Player 1 input (integer)
+	li $v0, 5
+	syscall
+	move $t0, $v0
+	
+	#loads base address of array into $s0
+	la $s3, board
+	
+	#calculate offset
+	sub $t0, $t0, 1
+	mul $t0, $t0, 4
+	
+	#increase base address to address user is looking for
+	add $s3, $s3, $t0
+	
+	#load position in array
+	lw $t1, 0($s3)
+	
+	#check that piece can be placed there
+	#jumps to wrongPostion1 label if position is taken
+	beq $t1, 1, wrongPosition1
+	beq $t1, 2, wrongPosition1
+	
+	#Update the board and check for winning condition if it passes the check
+    	sw $s1, 0($s3)
+    	printBoard
+    
+    	#ADD WINNING CONDITION CHECK
+    
+	#Increment loop counter
+	add $s0, $s0, 1
 
-    ### --- Implement a correct input checker here --- ###
-    ### --- Update the board and check for winning condition if it passes the check --- ###
+	### --- Draw condition will be implemented here --- ###
 
-    # Increment loop counter
-    add $s0, $s0, 1
-
-    ### --- Draw condition will be implemented here --- ###
-
-    # Player 2's turn
-    j player2
+	#Player 2's turn
+	j player2
 
 player2:
-    # Player 2 input (integer)
-    li $v0, 5
-    syscall
+	#prompt player 2
+	li $v0, 4
+	la $a0, prompt2
+	syscall
+	
+	#Player 2 input (integer)
+	li $v0, 5
+	syscall
+	move $t0, $v0
+	
+	#loads base address of array into $s0
+	la $s3, board
+	
+	#calculate offset
+	sub $t0, $t0, 1
+	mul $t0, $t0, 4
+	
+	#increase base address to address user is looking for
+	add $s3, $s3, $t0
+	
+	#load position in array
+	lw $t1, 0($s3)
+	
+	#check that piece can be placed there
+	#jumps to wrongPosition2 label if position is taken
+	beq $t1, 1, wrongPosition2
+	beq $t1, 2, wrongPosition2
+	
+	#Update the board and check for winning condition if it passes the check
+    	sw $s2, 0($s3)
+    	printBoard
+    
+    	#ADD WINNING CONDITION CHECK
+    
+	#Increment loop counter
+	add $s0, $s0, 1
 
-    ### --- Implement a correct input checker here --- ###
-    ### --- Update the board and check for winning condition if it passes the check --- ###
+	### --- Draw condition will be implemented here --- ###
 
-    # Increment loop counter
-    # addi $s0, 1
+	#Player 1's turn
+	j player1
 
-    ### -- Draw condition is not required to be implemented here since player 1 starts -- ##
 
-    # Player 1's turn
-    j player1
+wrongPosition1:
+	#inform user of error
+	li $v0, 4
+	la $a0, wrongPosition
+	syscall
+	
+	#jump back to player1 label
+	j player1
+	
+wrongPosition2:
+	#inform user of error
+	li $v0, 4
+	la $a0, wrongPosition
+	syscall
+	
+	#jump back to player2 label
+	j player2
 
+#Exit Program
+exit:
+	li $v0, 10
+	syscall
